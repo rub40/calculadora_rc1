7430c70e32f74996ab1cf150280274a01d0a1303
rub40 <joserubens.fo@gmail.com>
2021-12-01 00:18:29 -0300

    ajustes

--- src/Client.java
+++ src/Client.java
@@ -1,34 +1,37 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Client {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
 	public static void main(String[] args) throws IOException {
-
-		@SuppressWarnings("resource")
-		Scanner sc = new Scanner(System.in);
-		@SuppressWarnings("resource")
-		Socket s = new Socket("localhost", 9999);
-		boolean stop = false;
 		
-		DataInputStream dis = new DataInputStream(s.getInputStream());
-		DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+		boolean stop = false;
 		
 		while(!stop) {
 
+			Socket s = new Socket("localhost", 9999);
+			DataInputStream dis = new DataInputStream(new BufferedInputStream(s.getInputStream()));
+			DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(s.getOutputStream()));
+			
 			//Busca as informações da equação através do Scanner
 			System.out.println("Calculadora Cliente/Servidor:");
 			System.out.println("Digite a letra F para sair da calculadora.");
 			System.out.println("Digite a conta a ser feita: (Ex: primeiro_numero operador segundo_numero)");
 			
-			String calc = sc.nextLine();
+			String calc = br.readLine();
 			
 			//Finaliza as operações da calculadora ou Imprime o resultado da equação
 			if(!(calc.equals("F") || calc.equals("f"))) {
 				dos.writeUTF(calc);
+				dos.flush();
 				String result = dis.readUTF();
 				System.out.println("Resultado = " + result);
 			}else {
--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,5 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -9,53 +11,79 @@ public class Server {
 
 	public static void main(String[] args) throws IOException {
 		
-		@SuppressWarnings("resource")
-		ServerSocket ss = new ServerSocket(9999);
-		Socket s = ss.accept();
+		ServerSocket ss = null;
+		Socket s = null;
 		
-		//Pega as informações do input/output do Socket
-		DataInputStream dis = new DataInputStream(s.getInputStream());
-        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-        
+		try {
+			ss = new ServerSocket(9999);
+		}catch(Exception ex) {
+			System.err.println(ex);
+			return;
+		}
+		     
         while(true) {
+   	
+        	try {
         	
-        	String result = "";
-        	String calc = dis.readUTF();
-        	StringTokenizer st = new StringTokenizer(calc);
-
-        	//Finaliza o servidor atraves da informação que trazida do client
-        	if(calc.equals("F") || calc.equals("f")) {
-        		System.out.println("Servidor fora de operação (Parado pelo Client)");
-				break;
-			}
-        	        	
-        	//Monta a espressão e o calculo feito
-        	double num1 = Double.parseDouble(st.nextToken());
-        	String operator = st.nextToken();
-        	double num2 = Double.parseDouble(st.nextToken());
-        	
-        	switch(operator) {
-        	case "+":
-        		result = String.valueOf(num1 + num2);
-        		break;
-        	case "/":
-        		result = String.valueOf(num1 / num2);
-        		break;
-        	case "-":
-        		result = String.valueOf(num1 - num2);
-        		break;
-        	case "*":
-        		result = String.valueOf(num1 * num2);
-        		break;
-        	default:
-        		result = "Valores incorretos";
-        		break;
+        		s = ss.accept();
+        		
+        		//Pega as informações do input/output do Socket
+        		DataInputStream dis = new DataInputStream(new BufferedInputStream(s.getInputStream()));
+                DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(s.getOutputStream()));
+                
+            	String result = "";
+            	String calc = dis.readUTF();
+            	
+            	if(calc != null) {
+            		
+                	StringTokenizer st = new StringTokenizer(calc);
+                	
+                	//Monta a espressão e o calculo feito
+                	double num1 = Double.parseDouble(st.nextToken());
+                	String operator = st.nextToken();
+                	double num2 = Double.parseDouble(st.nextToken());
+                	
+                	switch(operator) {
+                	case "+":
+                		result = String.valueOf(num1 + num2);
+                		break;
+                	case "/":
+                		result = String.valueOf(num1 / num2);
+                		break;
+                	case "-":
+                		result = String.valueOf(num1 - num2);
+                		break;
+                	case "*":
+                		result = String.valueOf(num1 * num2);
+                		break;
+                	default:
+                		result = "Valores incorretos";
+                		break;
+                	}   		
+            	}
+            
+				//Escreve o resultado da operação na informação do socket
+				dos.writeUTF(result);
+				dos.flush();
+				
+				//Tenta fechar o socket
+				try {
+                    s.close();
+                }
+                catch (Exception ex) {
+                	
+                }
+            	                
+        	}catch(Exception ex) {
+        		if(s != null) {
+                    try {
+                        s.close();
+                    }
+                    catch (Exception ex2) {
+                    	
+                    }
+                }
         	}
-        	
-        	//Escreve o resultado da operação na informação do socket
-        	dos.writeUTF(result);
-        	dos.flush();
-
         }
 	}
 
